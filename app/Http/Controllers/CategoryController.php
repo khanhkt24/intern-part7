083a4d8ef65b6803123ab7f6085cf43032adcff3
Khổng Trọng Khánh <khanh130404@gmail.com>
2024-11-21 16:47:22 +0700

    product& category  controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Requests\UpdateCategoryRequest;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
@@ -14,6 +15,8 @@ class CategoryController extends Controller
     public function index()
     {
         //
+        $data = DB::table('categories')->latest('id')->paginate(10);
+        return view('admin.categories.index', compact('data'));
     }
 
     /**
@@ -22,6 +25,7 @@ class CategoryController extends Controller
     public function create()
     {
         //
+        return view('admin.categories.create');
     }
 
     /**
@@ -29,38 +33,74 @@ class CategoryController extends Controller
      */
     public function store(StoreCategoryRequest $request)
     {
-        //
+        $data =  $request->validate([
+            'name' => 'required|unique:categories,name'
+        ],
+        [
+            'unique'   => 'Danh mục đã tồn tại, vui lòng chọn tên danh mục khác.',
+            'required' => 'Tên danh mục không được để trống.',
+        ]
+    );
+
+        Category::create($data);
+
+        return redirect()->route('categories.index')->with('message', 'Thêm mới thành công');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Category $category)
+    public function show(Category $category, $id)
     {
         //
+        $category = DB::table('categories')->where('id', $id)->first();
+        return view('admin.categories.show', compact('category'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Category $category)
+    public function edit(Category $category, $id)
     {
         //
+        $category = DB::table('categories')->where('id', $id)->first();
+        return view('admin.categories.edit', compact('category'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCategoryRequest $request, Category $category)
+    public function update(StoreCategoryRequest $request, Category $category, $id)
     {
         //
+        $data =  $request->validate([
+            'name' => 'required|unique:categories,name'
+        ],
+        [
+            'unique'   => 'Danh mục đã tồn tại, vui lòng chọn tên danh mục khác.',
+            'required' => 'Tên danh mục không được để trống.',
+        ]
+    );
+        // DB::table('categories')->where('id', $id)->update([
+        //     'name' => $request->name
+        // ]);
+        $category ->where('id', $id)->update($data);
+        return redirect()->route('categories.index')->with('message', 'Cập nhật thành công');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Category $category)
+    public function destroy($id)
     {
-        //
+        $category = Category::find($id);
+
+        // Kiểm tra xem danh mục có sản phẩm hay không
+        if ($category->products()->count() > 0) {
+            return back()->with('error', 'Không thể xóa danh mục này vì có sản phẩm ở trong!');
+        }
+
+        $category->delete();
+        return back()->with('message', 'Xóa thành công.');
     }
 }
--- app/Http/Controllers/ProductSizeController.php
+++ app/Http/Controllers/ProductSizeController.php
@@ -13,7 +13,8 @@ class ProductSizeController extends Controller
      */
     public function index()
     {
-        //
+        $productSizes = ProductSize::query()->get();
+        return view('admin.productsizes.index', compact('productSizes'));
     }
 
     /**
@@ -21,7 +22,7 @@ class ProductSizeController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.productsizes.create');
     }
 
     /**
@@ -29,38 +30,43 @@ class ProductSizeController extends Controller
      */
     public function store(StoreProductSizeRequest $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(ProductSize $productSize)
-    {
-        //
+        $productSizes = ProductSize::create($request->all());
+        return redirect()->route('productsize.index');
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(ProductSize $productSize)
+    public function edit($id)
     {
-        //
+        $productSize = ProductSize::find($id);
+        return view('admin.productsizes.edit', compact('productSize'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateProductSizeRequest $request, ProductSize $productSize)
+    public function update(StoreProductSizeRequest $request, $id)
     {
-        //
+        $productSize = ProductSize::find($id);
+        $productSize->update([
+            'name' => $request->name
+        ]);
+        return redirect()->route('productsize.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(ProductSize $productSize)
+    public function destroy($id)
     {
-        //
+        $productSizes = ProductSize::find($id);
+        // $variants = Variant::query()->where('product_size_id', $id)->first();
+        // if ($variants) {
+        //     return redirect()->route('productsize.index')->withErrors(['size_error' => 'Kích thước "' . $productSizes->name . '" đang được sử dụng không thể xóa']);
+        // } else {
+        //     $productSizes->delete();
+            return redirect()->route('productsize.index');
+        // }
     }
 }
