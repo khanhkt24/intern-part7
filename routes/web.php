4814c4ff2066df674071c371658a8307d5e51405
DaoVietAnh79 <lupinanh2k4@gmail.com>
2024-11-23 21:52:55 +0700

    Controller and Route - Client

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\client\AccountController;
 use App\Http\Middleware\CheckRole;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Client
 
+Route::get('/login', [AccountController::class, 'login'])->name('login');
+Route::get('/verify-account/{email}', [AccountController::class, 'verify_account'])->name('verify.account');
+Route::post('/login', [AccountController::class, 'loginSubmit'])->name('loginSubmit');
+
+Route::get('/register', [AccountController::class, 'register'])->name('register');
+Route::post('/register', [AccountController::class, 'registerSubmit'])->name('registerSubmit');
+
+Route::get('/forgot-password', [AccountController::class, 'forgot_password'])->name('account.forgot_password');
+Route::post('/forgot-password', [AccountController::class, 'check_forgot_password']);
+
+Route::get('/reset-password/{token}', [AccountController::class, 'reset_password'])->name('account.reset_password');
+Route::post('/reset-password/{token}', [AccountController::class, 'check_reset_password']);
+
+Route::get('/logout', [AccountController::class, 'logout'])->name('logout');
