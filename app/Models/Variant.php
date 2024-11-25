c8b6aa1f36fdb51c451e132953f160ad99a95660
Khổng Trọng Khánh <khanh130404@gmail.com>
2024-11-25 08:46:42 +0700

    detail controller

--- app/Models/Variant.php
+++ app/Models/Variant.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\client\Cart;
+use App\Models\Cart;
 use App\Models\ProductSize;
 
 class Variant extends Model
