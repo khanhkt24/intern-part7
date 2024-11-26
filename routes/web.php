d6db0f754541e6fc1230ac66428aeae148b2ea63
Khổng Trọng Khánh <khanh130404@gmail.com>
2024-11-26 15:49:12 +0700

    add web

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,62 @@ Route::get('contact', function () {
 Route::get('shop-wishlist', function () {
     return view('client.shop-wishlist');
 });
+
+
+
+Route::middleware(CheckRole::class)->prefix('admin')->group(function () {
+
+
+    Route::get('/', [DashboardController::class, 'dashboard'])->name('dashboard');
+    Route::post('/statistics', [DashboardController::class, 'getStatistics'])->name('statistics');
+    Route::post('/statistics/year', [DashboardController::class, 'getStatisticsByYear'])->name('statisticsYear');
+    Route::post('/statistics-month', [DashboardController::class, 'statisticsMonth'])->name('statisticsMonth');
+    Route::post('/statistics/time-range', [DashboardController::class, 'getStatisticsByTimeRange'])->name('statisticsTimeRange');
+
+
+    Route::get('categories', [CategoryController::class, 'index'])->name('categories.index');
+    Route::get('categories/create', [CategoryController::class, 'create'])->name('categories.create');
+    Route::post('categories/store', [CategoryController::class, 'store'])->name('categories.store');
+    Route::get('categories/{id}/show', [CategoryController::class, 'show'])->name('categories.show');
+    Route::get('categories/{id}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+    Route::put('categories/{id}/update', [CategoryController::class, 'update'])->name('categories.update');
+    Route::delete('categories/{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+
+    // Route quản lý sản phẩm
+    Route::prefix('products')
+        ->as('products.')
+        ->group(function () {
+            Route::get('/',                     [ProductController::class, 'index'])->name('index');
+            Route::get('/create',               [ProductController::class, 'create'])->name('create');
+            Route::post('/store',               [ProductController::class, 'store'])->name('store');
+            Route::get('/show/{product}',       [ProductController::class, 'show'])->name('show');
+            Route::get('{product}/edit',        [ProductController::class, 'edit'])->name('edit');
+            Route::put('{product}/update',      [ProductController::class, 'update'])->name('update');
+            Route::delete('{product}/destroy',  [ProductController::class, 'destroy'])->name('destroy');
+        });
+
+    // Route quản lý biến thể sản phẩm
+    Route::prefix('variants')
+        ->as('variants.')
+        ->group(function () {
+            Route::get('/',                     [VariantController::class, 'index'])->name('index');
+            Route::get('/show/{variant}',       [VariantController::class, 'show'])->name('show');
+            Route::get('{variant}/edit',        [VariantController::class, 'edit'])->name('edit');
+            Route::put('{variant}/update',      [VariantController::class, 'update'])->name('update');
+            Route::delete('{variant}/destroy',  [VariantController::class, 'destroy'])->name('destroy');
+        });
+
+
+    // Route quản lý kích thước sản phẩm
+    Route::prefix('productsize')
+        ->as('productsize.')
+        ->group(function () {
+            Route::get('/',                     [ProductSizeController::class, 'index'])->name('index');
+            Route::get('/create',               [ProductSizeController::class, 'create'])->name('create');
+            Route::post('/store',               [ProductSizeController::class, 'store'])->name('store');
+            Route::get('/edit/{id}',            [ProductSizeController::class, 'edit'])->name('edit');
+            Route::put('/update/{id}',          [ProductSizeController::class, 'update'])->name('update');
+            Route::delete('/delete/{id}',       [ProductSizeController::class, 'destroy'])->name('destroy');
+        });
+
+});
